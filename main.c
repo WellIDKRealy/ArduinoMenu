a9ec15052889cb85ed093fbb66feffc443c3c1d2
Maciej Kalandyk <m.kalandyk@outlook.com>
2024-04-05 22:16:38 +0200

    to rebase

--- main.c
+++ main.c
@@ -56,6 +56,8 @@ void snake(void) {
   tail_x = TEXT_WIDTH/2;
   tail_y = TEXT_HEIGHT/2;
 
+  uint8_t snake_size = 1;
+
   apple_x = rand() % TEXT_WIDTH;
   apple_y = rand() % TEXT_HEIGHT;
 
@@ -71,6 +73,7 @@ void snake(void) {
 	  if(snake_direction != Down)
 		snake_direction = Up;
 	  break;
+	case '5':
 	case '8':
 	  if(snake_direction != Up)
 		snake_direction = Down;
@@ -115,15 +118,23 @@ void snake(void) {
 		break;
 	  }
 	  goto snake_lost;
+	case None:
+	  /* Will not ever happen */
+	  break;
 	}
 
 	if(head_x == apple_x && head_y == apple_y) {
+	  snake_size += 1;
+
+	  if(snake_size == TEXT_WIDTH*TEXT_HEIGHT) {
+		goto snake_won;
+	  }
+
 	  apple_x = rand() % TEXT_WIDTH;
 	  apple_y = rand() % TEXT_HEIGHT;
 
 	  lcd_gotoxy(apple_x, apple_y);
 	  lcd_putc('O');
-
 
 	  movement_map[tail_y*TEXT_WIDTH + tail_x] = snake_tail_dir;
 	  snake_tail_dir = None;
@@ -149,6 +160,9 @@ void snake(void) {
 	case Right:
 	  tail_x += 1;
 	  break;
+	case None:
+	  /* Will not ever happen */
+	  break;
 	}
 
 	lcd_gotoxy(head_x, head_y);
@@ -164,6 +178,9 @@ void snake(void) {
 	  break;
 	case Right:
 	  lcd_putc('>');
+	  break;
+	case None:
+	  /* Will not ever happen */
 	  break;
 	}
 
@@ -201,183 +218,183 @@ void snake(void) {
   return;
 }
 
-  void test(void) {
-	lcd_clear_buffer();
+void test(void) {
+  lcd_clear_buffer();
 
-	lcd_puts("Hello World");
-	lcd_display();
+  lcd_puts("Hello World");
+  lcd_display();
 
-	_delay_ms(1000);
+  _delay_ms(1000);
+}
+
+static menu_t GAMES_MENU = {
+  MenuType,
+  "Games",
+  0,
+  0,
+  8,
+  {
+	(menu_item_union_t)&(menu_program_t){MenuProgramType, "Snake", &snake},
+	(menu_item_union_t)&(menu_program_t){MenuProgramType, "Test", &test},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game2"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game3"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game4"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game5"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game6"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game7"},
+	(menu_item_union_t)&(menu_item_t){MenuItemType, "Game8"},
+  }
+};
+
+static menu_t DEFAULT_MENU = {
+  MenuType,
+  "Default",
+  0,
+  0,
+  1,
+  {
+	(menu_item_union_t)&GAMES_MENU,
+  }
+};
+
+static menu_t* SELECTED_MENU = &DEFAULT_MENU;
+static menu_stack_t* MENU_STACK = NULL;
+
+static inline void clear_selected(menu_t* menu) {
+  lcd_gotoxy(0, menu->selected - menu->origin + 1);
+  lcd_putc(' ');
+}
+
+static inline void draw_selected(menu_t* menu) {
+  lcd_gotoxy(0, menu->selected - menu->origin + 1);
+  lcd_putc('>');
+}
+
+static inline void draw_menu(menu_t* menu) {
+  lcd_clear_buffer();
+
+  // header
+  char header[TEXT_WIDTH];
+  sprintf(header, "menu: %s", menu->name);
+
+  lcd_gotoxy(0, 0);
+  lcd_puts(header);
+
+  for(uint8_t i = menu->origin; i < MIN(menu->size, menu->origin + TEXT_HEIGHT - 1); i++) {
+	lcd_gotoxy(1, i - menu->origin + 1);
+	lcd_puts(menu->items[i].menu_item->name);
   }
 
-  static menu_t GAMES_MENU = {
-	MenuType,
-	"Games",
-	0,
-	0,
-	8,
-	{
-	  (menu_item_union_t)&(menu_program_t){MenuProgramType, "Snake", &snake},
-	  (menu_item_union_t)&(menu_program_t){MenuProgramType, "Test", &test},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game2"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game3"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game4"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game5"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game6"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game7"},
-	  (menu_item_union_t)&(menu_item_t){MenuItemType, "Game8"},
-	}
-  };
+  draw_selected(menu);
+}
 
-  static menu_t DEFAULT_MENU = {
-	MenuType,
-	"Default",
-	0,
-	0,
-	1,
-	{
-	  (menu_item_union_t)&GAMES_MENU,
-	}
-  };
-
-  static menu_t* SELECTED_MENU = &DEFAULT_MENU;
-  static menu_stack_t* MENU_STACK = NULL;
-
-  static inline void clear_selected(menu_t* menu) {
-	lcd_gotoxy(0, menu->selected - menu->origin + 1);
-	lcd_putc(' ');
+static inline void inc_selected(menu_t* menu) {
+  if(menu->selected + 1 >= menu->size) {
+	return;
   }
 
-  static inline void draw_selected(menu_t* menu) {
-	lcd_gotoxy(0, menu->selected - menu->origin + 1);
-	lcd_putc('>');
+  clear_selected(menu);
+
+  menu->selected += 1;
+
+  if(menu->selected - menu->origin + 2 >= TEXT_HEIGHT - 1 && menu->size - menu->origin > TEXT_HEIGHT - 1) {
+	menu->origin += 1;
+
+	draw_menu(menu);
+	return;
   }
 
-  static inline void draw_menu(menu_t* menu) {
-	lcd_clear_buffer();
+  draw_selected(menu);
+}
 
-	// header
-	char header[TEXT_WIDTH];
-	sprintf(header, "menu: %s", menu->name);
-
-	lcd_gotoxy(0, 0);
-	lcd_puts(header);
-
-	for(uint8_t i = menu->origin; i < MIN(menu->size, menu->origin + TEXT_HEIGHT - 1); i++) {
-	  lcd_gotoxy(1, i - menu->origin + 1);
-	  lcd_puts(menu->items[i].menu_item->name);
-	}
-
-	draw_selected(menu);
+static inline void dec_selected(menu_t* menu) {
+  if(menu->selected <= 0) {
+	return;
   }
 
-  static inline void inc_selected(menu_t* menu) {
-	if(menu->selected + 1 >= menu->size) {
-	  return;
-	}
+  clear_selected(menu);
 
-	clear_selected(menu);
+  menu->selected -= 1;
 
-	menu->selected += 1;
+  if(menu->selected - menu->origin < 2 && menu->origin > 0) {
+	menu->origin -= 1;
 
-	if(menu->selected - menu->origin + 2 >= TEXT_HEIGHT - 1 && menu->size - menu->origin > TEXT_HEIGHT - 1) {
-	  menu->origin += 1;
-
-	  draw_menu(menu);
-	  return;
-	}
-
-	draw_selected(menu);
+	draw_menu(menu);
+	return;
   }
 
-  static inline void dec_selected(menu_t* menu) {
-	if(menu->selected <= 0) {
-	  return;
-	}
+  draw_selected(menu);
+}
 
-	clear_selected(menu);
+int main(void) {
+  keypad = make_keypad(COLS, COLS_NO, ROWS, ROWS_NO);
+  init_keypad(&keypad);
 
-	menu->selected -= 1;
+  lcd_init(LCD_DISP_ON);
 
-	if(menu->selected - menu->origin < 2 && menu->origin > 0) {
-	  menu->origin -= 1;
+  draw_menu(SELECTED_MENU);
+  lcd_display();
 
-	  draw_menu(menu);
-	  return;
-	}
+  bool changed;
 
-	draw_selected(menu);
-  }
+  while(true) {
+	changed = true;
 
-  int main(void) {
-	keypad = make_keypad(COLS, COLS_NO, ROWS, ROWS_NO);
-	init_keypad(&keypad);
+	current_mask = key_scan16(&keypad);
+	char pressed = key_get_first_char_diff(&keypad, cmap, current_mask, last_mask);
+	last_mask = current_mask;
 
-	lcd_init(LCD_DISP_ON);
-
-	draw_menu(SELECTED_MENU);
-	lcd_display();
-
-	bool changed;
-
-	while(true) {
-	  changed = true;
-
-	  current_mask = key_scan16(&keypad);
-	  char pressed = key_get_first_char_diff(&keypad, cmap, current_mask, last_mask);
-	  last_mask = current_mask;
-
-	  switch(pressed) {
-	  case '2':
-		dec_selected(SELECTED_MENU);
+	switch(pressed) {
+	case '2':
+	  dec_selected(SELECTED_MENU);
+	  break;
+	case '8':
+	  inc_selected(SELECTED_MENU);
+	  break;
+	case '4':
+	  if(MENU_STACK == NULL) {
 		break;
-	  case '8':
-		inc_selected(SELECTED_MENU);
-		break;
-	  case '4':
-		if(MENU_STACK == NULL) {
-		  break;
-		}
+	  }
 
-		SELECTED_MENU = MENU_STACK->menu;
+	  SELECTED_MENU = MENU_STACK->menu;
 
-		menu_stack_t* back = MENU_STACK->back;
-		free(MENU_STACK);
-		MENU_STACK = back;
+	  menu_stack_t* back = MENU_STACK->back;
+	  free(MENU_STACK);
+	  MENU_STACK = back;
+
+	  draw_menu(SELECTED_MENU);
+
+	  break;
+	case '6':
+	  menu_item_union_t val = SELECTED_MENU->items[SELECTED_MENU->selected];
+
+	  switch(val.menu_item->type) {
+	  case MenuType:
+		MENU_STACK = &(menu_stack_t){MENU_STACK, SELECTED_MENU};
+		SELECTED_MENU = val.menu;
 
 		draw_menu(SELECTED_MENU);
-
 		break;
-	  case '6':
-		menu_item_union_t val = SELECTED_MENU->items[SELECTED_MENU->selected];
 
-		switch(val.menu_item->type) {
-		case MenuType:
-		  MENU_STACK = &(menu_stack_t){MENU_STACK, SELECTED_MENU};
-		  SELECTED_MENU = val.menu;
+	  case MenuProgramType:
+		val.menu_program->program_f();
 
-		  draw_menu(SELECTED_MENU);
-		  break;
-
-		case MenuProgramType:
-		  val.menu_program->program_f();
-
-		  draw_menu(SELECTED_MENU);
-		  break;
-
-		default:
-		  break;
-		}
+		draw_menu(SELECTED_MENU);
 		break;
 
 	  default:
-		changed = false;
+		break;
 	  }
+	  break;
 
-	  if(changed) {
-		lcd_display();
-	  }
-
-	  _delay_ms(10);
+	default:
+	  changed = false;
 	}
+
+	if(changed) {
+	  lcd_display();
+	}
+
+	_delay_ms(10);
   }
+}
